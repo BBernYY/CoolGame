4bf8948e19f42e17b325caee7f766df9303a7873
BBernYY <66414852+BBernYY@users.noreply.github.com>
2022-02-21 18:00:06 +0100

    coded reforge and weapon rolls

--- main.py
+++ main.py
@@ -1,20 +1,60 @@
-from json import dump
-import utils
+def combine_dict(data, changes):
+    for k, v in data.items():
+        if k in changes:
+            if not isinstance(changes[k], str):
+                data[k] += changes[k]
 
 def save_game(changes):
-    dump(changes, open('data.json', 'w'))
+    from json import dump, load
+    data = load(open('data.json', 'r'))
+    data = combine_dict(data, changes)
+    dump(data, open('data.json', 'w'), indent=2)
 
+def roll_weapon(current_cash):
+    from random import randint, choice
+    from json import load
+    rarities = {
+        "LEGENDARY":95, "RARE":75, "UNCOMMON":50, "COMMON":0
+    }
+    roll = randint(0, 100)
+    for k, v in rarities.items():
+        if roll > v:
+            rarity = k
+            break
+    pool = []
+    for i in load(open("weapons.json", 'r')).keys():
+        if load(open("weapons.json", 'r'))[i]['rarity'] == rarity:
+            pool.append(load(open("weapons.json", 'r'))[i])
+    return {"cash": current_cash - 100, "weapons": [choice(pool)]}
 
+def roll_reforge(current_cash):
+    from random import randint, choice
+    from json import load
+    rarities = {
+        "COMMON":0, "UNCOMMON":50, "RARE":75, "LEGENDARY":95
+    }
+    roll = randint(0, 100)
+    for k, v in rarities.items():
+        if roll > v:
+            rarity = k
+            break
+    pool = []
+    for i in load(open("reforges.json", 'r')).keys():
+        if load(open("reforges.json", 'r'))[i]['rarity'] == rarity:
+            pool.append(load(open("reforges.json", 'r'))[i])
+    return {"cash": current_cash - 100, "weapons": [choice(pool)]}
+    
 def execute_command(command, *args):
     if command == 'fight':
         fight(*args)
     elif command == 'roll':
         if args[0] == 'weapon':
-            roll_weapon(*args[1:])
+            out = roll_weapon(*args[1:])
         elif args[0] == 'reforge':
-            roll_reforge(*args[1:])
+            out = roll_reforge(*args[1:])
     elif command == 'respond':
-        print(*args)
+        out = " ".join(args)
+    return out
 
 def main():
     import utils # imports my customized utils module, with a test and timing function. https://GitHub.com/BBernYY/FancyCoding
--- main_test.py
+++ main_test.py
@@ -1,2 +1,5 @@
 import main
-main.execute_command(*"respond Hello World!".split(" "))
+main.save_game({"cash": 50})
+x = main.roll_weapon(150)['weapons'][0]
+x['reforge'] = main.roll_reforge(150)
+print(x)
